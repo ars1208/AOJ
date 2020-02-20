e78676b3e7f9485c88282074b0473ca30b1bb2b8
ars1208 <ara.kaz1208@gmail.com>
2020-02-21 00:10:56 +0900

    itp1_1 commit

---
+++ ITP1/itp1_1_c.py
@@ -0,0 +1,6 @@
+a, b = map(int,input().split())
+
+area = a * b
+prm = 2*a + 2*b
+
+print(area, prm)
---
+++ ITP1/itp1_1_d.py
@@ -0,0 +1,8 @@
+s = int(input())
+
+h = s // 3600
+s = s - 3600*h
+m = s // 60
+s = s - 60*m
+
+print(str(h) + ":" + str(m) + ":" + str(s))
