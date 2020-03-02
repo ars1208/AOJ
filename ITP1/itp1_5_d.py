f14c2301cc86c6b592871c270a4fb25c649b7757
ars1208 <ara.kaz1208@gmail.com>
2020-03-02 18:18:18 +0900

    itp1_5_d commit

--- ITP1/itp1_5_d.py
+++ ITP1/itp1_5_d.py
@@ -1,1 +1,12 @@
 n = int(input())
+ans_list = list()
+
+for i in range(1,n+1):
+    if i % 3 == 0:
+        ans_list.append(str(i))
+    elif i % 10 == 3:
+        ans_list.append(str(i))
+    elif '3' in str(i):
+        ans_list.append(str(i))
+
+print('', ' '.join(ans_list))
