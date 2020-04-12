20ac7cb2aa4f8207db64ae857ac1e2ce0c69a06c
ars1208 <ara.kaz1208@gmail.com>
2020-04-12 19:35:09 +0900

    fix code

--- ITP2/itp2_1_b.py
+++ ITP2/itp2_1_b.py
@@ -1,24 +1,26 @@
+from collections import deque
+
 q = int(input())
 data_list = list()
 for i in range(q):
     data = input()
     data_list.append(data)
 
-a = list()
+a = deque()
 for data in data_list:
     if data[0] == "0":
         if data.split()[1] == "1":
             a.append(data.split()[2])
         else:
-            x = [data.split()[2]]
-            a = x + a
+            x = data.split()[2]
+            a.appendleft(x)
         # print(a)
     elif data[0] == "1":
         print(a[int(data.split()[1])])
         # print(a)
     else:
-        if data.split()[1] == "0":
-            a.pop(0)
+        if data.split()[1] == "1":
+            a.pop()
         else:
-            a.pop(-1)
+            a.popleft()
         # print(a)
